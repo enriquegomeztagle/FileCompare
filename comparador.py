bc67f29d66d5c0689020b44f1dca9d44cc51c47a
Lolo <0212511@up.edu.mx>
2024-07-11 16:44:22 -0600

    First approach and V0

---
+++ comparador.py
@@ -0,0 +1,65 @@
+import streamlit as st
+import pandas as pd
+
+def load_data(uploaded_file, delimiter, encoding):
+    if uploaded_file is not None:
+        try:
+            return pd.read_csv(uploaded_file, delimiter=delimiter, encoding=encoding)
+        except Exception as e:
+            st.error(f"Error al procesar el archivo: {e}")
+    return None
+
+
+def compare_dataframes(df1, df2):
+    col1, col2 = st.columns(2)  # Crear dos columnas
+    
+    with col1:  # Información del Primer DataFrame en la primera columna
+        st.write("### Primer DataFrame")
+        st.write(f"Longitud del DataFrame: {len(df1)}")
+        st.write("Columnas:", df1.columns.tolist())
+    
+    with col2:  # Información del Segundo DataFrame en la segunda columna
+        st.write("### Segundo DataFrame")
+        st.write(f"Longitud del DataFrame: {len(df2)}")
+        st.write("Columnas:", df2.columns.tolist())
+
+    # Comparar si las filas son exactamente iguales
+    if df1.equals(df2):
+        st.success("Los archivos son idénticos!")
+    else:
+        st.warning("Los archivos tienen diferencias.")
+
+def main():
+    st.title('Comparador de Archivos')
+    
+    # Crear columnas para los cargadores de archivo
+    col1, col2 = st.columns(2)
+    
+    with col1:
+        file1 = st.file_uploader("Carga el primer archivo", type=['csv', 'txt'], key='file1')
+        delimiter1 = st.selectbox("Selecciona el delimitador para el primer archivo", 
+                                  options=[',', '|', ';', '\t'], index=0, key='delim1')
+        encoding1 = st.selectbox("Selecciona la codificación para el primer archivo",
+                                 options=['utf-8', 'latin-1'], index=0, key='enc1')
+
+    with col2:
+        file2 = st.file_uploader("Carga el segundo archivo", type=['csv', 'txt'], key='file2')
+        delimiter2 = st.selectbox("Selecciona el delimitador para el segundo archivo", 
+                                  options=[',', '|', ';', '\t'], index=0, key='delim2')
+        encoding2 = st.selectbox("Selecciona la codificación para el segundo archivo",
+                                 options=['utf-8', 'latin-1'], index=0, key='enc2')
+    
+    if st.button("Comparar archivos"):
+        if file1 is not None and file2 is not None:
+            # Cargar los dataframes
+            df1 = load_data(file1, delimiter1, encoding1)
+            df2 = load_data(file2, delimiter2, encoding2)
+            
+            if df1 is not None and df2 is not None:
+                # Comparar dataframes
+                compare_dataframes(df1, df2)
+        else:
+            st.error("Por favor, carga ambos archivos para proceder con la comparación.")
+
+if __name__ == "__main__":
+    main()
