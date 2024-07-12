6f27a593db833d41f11bf5139e1cce7914a7bcd7
Enrique Ulises Báez Gómez Tagle <0241823@up.edu.mx>
2024-07-11 21:23:38 -0600

    feat: add file preview before comparison

--- comparador.py
+++ comparador.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 
-# Función para leer el archivo y actualizar la barra de progreso
 def load_data_with_progress(uploaded_file, delimiter, encoding):
     if uploaded_file is not None:
         if uploaded_file.name.endswith(('.csv', '.txt')):
@@ -12,7 +11,6 @@ def load_data_with_progress(uploaded_file, delimiter, encoding):
                     progress_bar = st.progress(0)
                     df = pd.DataFrame()
                     
-                    # Leer el archivo en partes y actualizar la barra de progreso
                     for i, chunk in enumerate(pd.read_csv(uploaded_file, delimiter=delimiter, encoding=encoding, chunksize=1000)):
                         df = pd.concat([df, chunk])
                         progress = min((i + 1) * chunk.memory_usage(deep=True).sum() / file_size, 1.0)
@@ -25,7 +23,7 @@ def load_data_with_progress(uploaded_file, delimiter, encoding):
             st.error("Formato de archivo no soportado. Por favor, carga un archivo CSV o TXT.")
     return None
 
-# Función para comparar los dataframes
+
 def compare_dataframes(df1, df2):
     col1, col2 = st.columns(2)
     
@@ -44,7 +42,7 @@ def compare_dataframes(df1, df2):
     else:
         st.warning("Los archivos tienen diferencias.")
 
-# Función principal de la aplicación
+
 def main():
     st.title('Comparador de Archivos')
     
@@ -57,7 +55,7 @@ def main():
         encoding1 = st.selectbox("Selecciona la codificación para el primer archivo",
                                  options=['utf-8', 'latin-1'], index=0, key='enc1')
         
-        # Previsualización del primer archivo
+
         if file1 is not None:
             df1_preview = load_data_with_progress(file1, delimiter1, encoding1)
             if df1_preview is not None:
@@ -71,7 +69,7 @@ def main():
         encoding2 = st.selectbox("Selecciona la codificación para el segundo archivo",
                                  options=['utf-8', 'latin-1'], index=0, key='enc2')
         
-        # Previsualización del segundo archivo
+
         if file2 is not None:
             df2_preview = load_data_with_progress(file2, delimiter2, encoding2)
             if df2_preview is not None:
